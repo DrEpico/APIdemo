8fdb941d8118724b53e00c8805a8eaad409eef71
Dr.Epico <adadashipour@gmail.com>
2024-06-08 15:47:34 +0330

    Add search feature

--- src/index.js
+++ src/index.js
@@ -7,20 +7,18 @@ searchDiv.appendChild(searchBox);
 searchDiv.appendChild(searchButton);
 body.appendChild(searchDiv);
 
-// searchButton.addEventListener();
+searchButton.addEventListener('click', function(){
+    searchContent = searchBox.value.trim();
+    getCats(searchContent);
+});
 
 let img = document.createElement('img');
 img.setAttribute('src', '#');
 
-async function getCats(){
-    const response = await fetch('https://api.giphy.com/v1/gifs/translate?api_key=GH3b8W0axp1F1Iy69HGH9GuU0Za4fB3a&s=cats', {mode: 'cors'});
+async function getCats(searchContent){
+    const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=GH3b8W0axp1F1Iy69HGH9GuU0Za4fB3a&s=${searchContent}`, {mode: 'cors'});
     const catData = await response.json();
     img.src = catData.data.images.original.url;
     console.log(catData.data.images.original.url);
     body.appendChild(img);
 }
-getCats();
-
-
-
-// free api key, don't bother
