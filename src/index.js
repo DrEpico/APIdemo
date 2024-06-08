40212bd3a547f83385be1c9213c214710727e8d7
Dr.Epico <adadashipour@gmail.com>
2024-06-08 12:38:06 +0330

    Add search box

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
+const body = document.querySelector('body');
+let searchDiv = document.createElement('div');
+let searchBox = document.createElement('input');
+let searchButton = document.createElement('button');
+searchButton.textContent = "Search";
+searchDiv.appendChild(searchBox);
+searchDiv.appendChild(searchButton);
+body.appendChild(searchDiv);
 
+searchButton.addEventListener();
+
+let image = document.createElement('img');
+image.setAttribute('src', '#');
+body.appendChild(img);
+
+
+//'https://api.giphy.com/v1/gifs/translate?api_key=GH3b8W0axp1F1Iy69HGH9GuU0Za4fB3a&s=cats'
+//free api key, don't bother
